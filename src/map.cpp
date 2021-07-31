b3d419b3e004215a9df3e09a8ecad82bb8f83122
Gienczyslaw <pewienjaaa@gmail.com>
2021-07-31 22:39:55 +0200

    📝 Relicensed all software.

--- src/map.cpp
+++ src/map.cpp
@@ -1,17 +1,7 @@
 // This file is part of Mactangle.
-// 
-// Mactangle is free software : you can redistribute it and /or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-// 
-// Mactangle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
-// GNU General Public License for more details.
-// 
-// You should have received a copy of the GNU General Public License
-// along with Mactangle. If not, see < https://www.gnu.org/licenses/>.
+// Mactangle is free software distributed under Mozilla Public License Version 2.0.
+// You should have received a copy of the Mozilla Public License 
+// along with Mactangle. If not, see <https://www.mozilla.org/en-US/MPL/2.0/>.
 
 #include "Map.h"
 
